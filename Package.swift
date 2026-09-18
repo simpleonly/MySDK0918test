5bbc159ad830d62eb2726aaae21ad5d3414053ea
simpleonly <2517077936@qq.com>
2026-09-18 11:35:27 +0800

    Update binary target URL for MyLibrary

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MyLibrary",
-            url: "https://github.com/simpleonly/SPMTest/releases/download/1.0.0/MyLibrary.zip",
+            url: "https://github.com/simpleonly/MySDK0918test/releases/download/1.0.0/MyLibrary.zip",
             checksum: "8e19691970312c706bc84b5771c285eb1d3c4ea1a31e1f02b6d01cd162570f13"
         )
     ]
