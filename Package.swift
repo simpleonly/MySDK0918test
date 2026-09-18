0e0a26a37ae313ca3ec40828fbb5266931571aa2
simpleonly <2517077936@qq.com>
2026-09-18 11:26:51 +0800

    Add Package.swift for MyLibrary package

---
+++ Package.swift
@@ -0,0 +1,18 @@
+import PackageDescription
+
+let package = Package(
+    name: "MyLibrary",
+    platforms: [
+        .iOS(.v13) // 根据你 SDK 支持的最低版本调整
+    ],
+    products: [
+        .library(name: "MyLibrary", targets: ["MyLibrary"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MyLibrary",
+            url: "https://github.com/simpleonly/SPMTest/releases/download/1.0.0/MyLibrary.zip",
+            checksum: "8e19691970312c706bc84b5771c285eb1d3c4ea1a31e1f02b6d01cd162570f13"
+        )
+    ]
+)
